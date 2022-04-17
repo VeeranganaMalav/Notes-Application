82b797353f69d3edce4c8fa81b65501bfb1ec988
VeeranganaMalav <veeranganamalav01@gmail.com>
2022-04-17 17:36:46 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,87 @@
+console.log("Welcome to Notes App.This is app.js");
+showNotes();
+
+
+//If user add a note, add it to localStorage
+let addBtn = document.getElementById('addBtn');
+addBtn.addEventListener("click", function(e){
+    let addTxt = document.getElementById('addTxt');
+    let notes = localStorage.getItem("notes");
+
+    if(notes == null){
+        notesObj = [];
+    }
+    else{
+        notesObj = JSON.parse(notes);
+    }
+
+    notesObj.push(addTxt.value);
+    localStorage.setItem("notes", JSON.stringify(notesObj));
+    addTxt.value = "";
+    showNotes();
+});
+
+
+//function to show elements from local storage
+function showNotes(){
+    let notes = localStorage.getItem("notes");
+    if(notes == null){
+        notesObj = [];
+    }
+    else{
+        notesObj = JSON.parse(notes);
+    }
+
+    let html = "";
+    notesObj.forEach(function(element, index){
+        html += `
+                <div class="noteCard my-2 mx-2 card" style="width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">Note ${index+1}</h5>
+                        <p class="card-text">${element}</p>
+                        <button id="${index}" onClick="deleteNote(this.id)" class="btn btn-primary">Delete Note</button>
+                    </div>
+                </div>
+                `;
+    });
+
+    let notesElem = document.getElementById('notes');
+    if(notes.length != 0){
+        notesElem.innerHTML = html;
+    }
+    else{
+        notesElem.innerHTML = `Nothing to show! Use "Add a note" section above to add notes`;
+    }
+}
+
+//function to delete a note
+function deleteNote(index){
+
+    let notes = localStorage.getItem("notes");
+    if(notes == null){
+        notesObj = [];
+    }
+    else{
+        notesObj = JSON.parse(notes);
+    }
+
+    notesObj.splice(index, 1);
+    localStorage.setItem("notes", JSON.stringify(notesObj));
+    showNotes();
+};
+
+let searchTxt = document.getElementById('searchTxt');
+searchTxt.addEventListener("input", function(){
+    inputVal = searchTxt.value.toLowerCase();
+
+    let noteCards = document.getElementsByClassName('noteCard');
+    Array.from(noteCards).forEach(function(element){
+        let cardTxt = element.getElementsByTagName('p')[0].innerText;
+        if(cardTxt.includes(inputVal)){
+            element.style.display = "block";
+        }
+        else{
+            element.style.display = "none";
+        }
+    });
+});
